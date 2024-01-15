87aaeed32b7069d16ca69b3e66bec03b0ec9b75f
Adriano047 <adrianocardoso047@gmail.com>
2024-01-15 15:40:41 -0300

    modificações na estlização

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+// configuração do slider----------------------------------
 var swiper = new Swiper(".swiper", {
     cssMode: true,
     loop: true,
@@ -10,3 +11,21 @@ var swiper = new Swiper(".swiper", {
     },
     keyboard: true,
   });
+// configuração do efeito maquina de escrever--------------------
+document.addEventListener('DOMContentLoaded', function() {
+  // O texto que será "digitado"
+  var texto = "Adriano Cardoso";
+  var span = document.getElementById('textoMaquina');
+
+  // Definindo o texto no span
+  span.textContent = '';
+
+  // Adicionando cada letra do texto com um pequeno atraso para simular a "digitação"
+  for (var i = 0; i < texto.length; i++) {
+      setTimeout(function() {
+          span.textContent += texto.charAt(0);
+          texto = texto.substring(1);
+      }, i * 100); // Atraso de 100ms entre cada letra
+  }
+});
+
